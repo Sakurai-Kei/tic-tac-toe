b17ac96ef32f7f3a9a936910c44c33d38de78c24
sakurai-kei <luqmanulraiserhakim@gmail.com>
2021-10-29 18:35:43 +0800

    The game now checks the board after every input to determine if there's a winner. Some of the code are restructured better to fit module pattern.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const Player = (_name, _sign) => {
     this._name = _name;
     this._sign = _sign;
-
     const getName = () => {
         return _name;
     };
-
     const getSign = () => {
         return _sign;
     };
-
     return {getName, getSign};
 };
 
@@ -20,9 +17,9 @@ const gameArea = (() => {
         return _move;
     }
     _setPlayArea.forEach(box => {
-        box.addEventListener('click', _setAreaSign)
+        box.addEventListener('click', setAreaSign)
     });
-    function _setAreaSign() {
+    function setAreaSign() {
         if(this.textContent !== ""){
             return;
         };
@@ -30,14 +27,15 @@ const gameArea = (() => {
             case 1:
                 this.textContent = `${playerX.getSign()}`;
                 _move++;
+                gameRules.winCondition();
                 break;
             case 0:
                 this.textContent = `${playerO.getSign()}`;
                 _move++;
+                gameRules.winCondition();
                 break;
         };
     };
-
     function getAreaSign() {
         let _playAreaSign = [];
         _setPlayArea.forEach(box => {
@@ -46,18 +44,23 @@ const gameArea = (() => {
         return _playAreaSign;
     }
 
-    function resetAreaSign() {
+    function _resetAreaSign() {
         _move = 1;
         _setPlayArea.forEach(box => {
             box.textContent = "";
         })
+        gameRules.startInput();
     }
-    return {getMove,getAreaSign, resetAreaSign}
+    const resetButton = document.getElementById('restart');
+    resetButton.addEventListener('click', _resetAreaSign);
+    return {setAreaSign, getMove,getAreaSign}
 })();
 
-const gameInitialise = (() => {
+const gameRules = (() => {
+    const _setPlayArea = Array.from(document.querySelectorAll('.column'));
+    let draw = true;
     function winCondition(){
-        let _playAreaSign = gameArea.getAreaSign();
+        const _playAreaSign = gameArea.getAreaSign();
         function _playerInputCombined(){
             let _row1 = [_playAreaSign[0], _playAreaSign[1], _playAreaSign[2]];
             let _row2 = [_playAreaSign[3], _playAreaSign[4], _playAreaSign[5]];
@@ -78,9 +81,13 @@ const gameInitialise = (() => {
             let _searchX = row.filter(_sign => _sign == "X");
             let _searchO = row.filter(_sign => _sign == "O");
             if(_searchX.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerX wins?`);
             }
             else if(_searchO.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerO wins?`);
             }
         })
@@ -88,9 +95,13 @@ const gameInitialise = (() => {
             let _searchX = column.filter(_sign => _sign == "X");
             let _searchO = column.filter(_sign => _sign == "O");
             if(_searchX.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerX wins?`);
             }
             else if(_searchO.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerO wins?`);
             }
         })
@@ -98,23 +109,31 @@ const gameInitialise = (() => {
             let _searchX = diagonal.filter(_sign => _sign =="X");
             let _searchO = diagonal.filter(_sign => _sign == "O");
             if(_searchX.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerX wins?`);
             }
             else if(_searchO.length == 3){
+                draw = false;
+                _stopInput();
                 return console.log(`playerO wins?`);
             }
         })
-        if(gameArea.getMove() == 10){
+        if(gameArea.getMove() == 10 & draw == true){
             return console.log(`Draw`);
         }
     }
-    return {winCondition}
+    function startInput() {
+        _setPlayArea.forEach(box => {
+        box.addEventListener('click', gameArea.setAreaSign)
+        })
+    }
+    function _stopInput() {
+        _setPlayArea.forEach(box => {
+        box.removeEventListener('click', gameArea.setAreaSign)
+        })
+    }
+    return {winCondition, startInput}
 })();
-
-
-
-const resetButton = document.getElementById('restart');
-resetButton.addEventListener('click', gameArea.resetAreaSign);
-
 const playerX = Player("Sakurai", "X");
 const playerO = Player("Cipher", "O");
